0940ff1f332253c68d4c118e4b1d38937c860e68
srx <srx92@protonmail.com>
2024-10-30 22:57:23 +0200

    Solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,38 @@
 import pytest
+
 from app.restore_names import restore_names
 
-# write your tests here
+
+correct_list = [
+    {
+        "first_name": "John",
+        "last_name": "Smith",
+        "full_name": "John Smith"
+    }
+]
+
+
+@pytest.fixture()
+def persons_template() -> list:
+    return [
+        {
+            "last_name": "Smith",
+            "full_name": "John Smith"
+        }
+    ]
+
+
+def test_should_add_the_first_name(
+        persons_template: list[dict]
+) -> None:
+    restore_names(persons_template)
+    assert persons_template == correct_list
+
+
+def test_restore_the_first_name_if_value_is_none(
+        persons_template: list[dict]
+) -> None:
+    person = persons_template
+    person[0]["first_name"] = None
+    restore_names(persons_template)
+    assert persons_template == correct_list
